3a6669c9a09947a386e8709f2a23186f7a7da1ab
song <only1711@naver.com>
2016-10-01 15:11:44 +0900

    메인 탭 적용

--- app/src/main/java/com/example/song/newsnts/MainActivity.java
+++ app/src/main/java/com/example/song/newsnts/MainActivity.java
@@ -1,25 +1,73 @@
 package com.example.song.newsnts;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-Button button_js;
+    Toolbar toolbar;
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+    Fragment4 fragment4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_js = (Button) findViewById(R.id.button_js);
-        button_js.setOnClickListener(new View.OnClickListener() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("친구"));
+        tabs.addTab(tabs.newTab().setText("채팅"));
+        tabs.addTab(tabs.newTab().setText("뉴스피드"));
+        tabs.addTab(tabs.newTab().setText("My Plan"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent3 = new Intent(getBaseContext(),com.example.song.newsnts.chat.class);
-                startActivity(intent3);
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭 : " + position);
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = fragment1;
+                } else if (position == 1) {
+                    selected = fragment2;
+                } else if (position == 2) {
+                    selected = fragment3;
+                } else if (position == 3) {
+                    selected = fragment4;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
             }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
         });
+
     }
 }
